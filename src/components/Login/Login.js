720d14b8c095a716f93533664b1ebd26eb5d1954
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-04-16 06:19:05 +0600

    user create by email and password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../images/logo/logo2.png";
 import "./Login.css";
 import googleLogo from "../../images/google.png";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
   return (
     <div className="input-container">
       <img src={logo} alt="" />
@@ -12,13 +23,29 @@ const Login = () => {
         <form>
           <h3>Please Login</h3>
           <input type="text" placeholder="Name" />
-          <input type="email" name="" id="" placeholder="Email" />
-          <input type="password" name="" id="" placeholder="Password" />
+          <input
+            onBlur={handleEmailBlur}
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+            required
+          />
+          <input
+          onBlur={handlePasswordBlur}
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+            required
+          />
           <input type="submit" value="Login" />
           <button>Login</button>
         </form>
-        <p className="navigate-to-signUp">New to Red Onion? <Link to="/signUp">Create an Account</Link></p>
-        <div >
+        <p className="navigate-to-login">
+          New to Red Onion? <Link to="/signUp">Create an Account</Link>
+        </p>
+        <div>
           <button className="google-button">
             <img src={googleLogo} alt="" /> <p>Countinue With Google</p>
           </button>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useState } from "react";
+import logo from "../../images/logo/logo2.png";
+import googleLogo from "../../images/google.png";
+import { Link, useNavigate } from "react-router-dom";
+import "./SignUp.css";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../fiebase.init";
 
 const SignUp = () => {
-    return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordBlur = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+
+if(user){
+    navigate('/')
+}
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Your password did not match");
+      return;
+    }
+    if (password.length < 6) {
+      setError("password must be 6 characters or longer");
+    }
+
+
+    createUserWithEmailAndPassword(email, password);
+  };
+  return (
+    <div className="input-container">
+      <img src={logo} alt="" />
+      <div className="input-area">
+        <form onSubmit={handleCreateUser}>
+          <h3>Please SignUp</h3>
+          <input type="text" placeholder="Name" />
+          <input
+            onBlur={handleEmailBlur}
+            type="email"
+            name=""
+            id=""
+            placeholder="Email"
+            required
+          />
+          <input
+            onBlur={handlePasswordBlur}
+            type="password"
+            name=""
+            id=""
+            placeholder="Password"
+            required
+          />
+          <input
+            onBlur={handleConfirmPasswordBlur}
+            type="password"
+            name=""
+            id=""
+            placeholder=" Confirm Password"
+            required
+          />
+          <p style={{ color: "red" }}>{error}</p>
+
+          <button>SignUp</button>
+        </form>
+        <p className="navigate-to-signUp">
+          Already have an Account? <Link to="/login">Login</Link>
+        </p>
         <div>
-            <h1>this si signUp</h1>
+          <button className="google-button">
+            <img src={googleLogo} alt="" /> <p>Countinue With Google</p>
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
